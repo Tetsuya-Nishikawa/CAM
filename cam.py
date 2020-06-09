bd4048e8573b40835e876100b34beb8bf640f54e
Tetsuya-Nishikawa <61189782+Tetsuya-Nishikawa@users.noreply.github.com>
2020-06-09 15:50:41 +0900

    Update cam.py

--- cam.py
+++ cam.py
@@ -3,6 +3,77 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 import tensorflow as tf
+import seaborn as sns
+
+%matplotlib inline
+
+tf.random.set_seed(
+    1234
+)
+
+def tensor_cast(inputs, labels):
+    inputs = tf.cast(inputs, tf.float32)
+    return inputs, tf.cast(labels, tf.int64)
+
+class Model(tf.keras.Model):
+
+    def __init__(self):
+        super(Model, self).__init__()
+        self.layer1 = tf.keras.layers.BatchNormalization(input_shape=(200, 220))
+        self.layer2 = tf.keras.layers.Conv2D(16, 3, padding='same', kernel_initializer="he_normal",activation='relu')
+        self.layer3 = tf.keras.layers.AveragePooling2D()
+        self.layer4 = tf.keras.layers.BatchNormalization()
+        self.layer5 = tf.keras.layers.Conv2D(32, 3, padding='same', kernel_initializer="he_normal",activation='relu')
+        self.layer6 = tf.keras.layers.AveragePooling2D()
+        self.layer7 = tf.keras.layers.BatchNormalization()
+        self.layer8 = tf.keras.layers.Conv2D(64, 3, padding='same', kernel_initializer="he_normal",activation='relu')
+        self.layer9 = tf.keras.layers.AveragePooling2D()
+        self.layer10 = tf.keras.layers.BatchNormalization()
+        self.layer11 = tf.keras.layers.Conv2D(128, 3, padding='same', kernel_initializer="he_normal",activation='relu')
+        self.layer12 = tf.keras.layers.AveragePooling2D()
+        self.layer13 = tf.keras.layers.BatchNormalization()
+        self.layer14 = tf.keras.layers.Conv2D(256, 3, padding='same', kernel_initializer="he_normal",activation='relu')
+        self.layer15 = tf.keras.layers.GlobalAveragePooling2D()
+
+        self.layer16 = tf.keras.layers.Flatten()
+        self.layer17 = tf.keras.layers.Dense(2, activation='softmax')
+
+    def call(self, inputs):
+        inputs   = self.layer1(inputs)
+        inputs   = self.layer2(inputs)
+        inputs   = self.layer3(inputs)
+        inputs   = self.layer4(inputs)
+        inputs   = self.layer5(inputs)
+        inputs   = self.layer6(inputs)
+        inputs   = self.layer7(inputs)
+        inputs   = self.layer8(inputs)
+        inputs   = self.layer9(inputs)
+        inputs   = self.layer10(inputs)
+        inputs   = self.layer11(inputs)
+        inputs   = self.layer12(inputs)
+        inputs   = self.layer13(inputs)
+        inputs   = self.layer14(inputs)
+        inputs   = self.layer15(inputs)
+        inputs   = self.layer16(inputs)
+        outputs = self.layer17(inputs)
+        return outputs
+    
+    def get_map(self, inputs):
+        inputs = self.layer1(inputs)
+        inputs = self.layer2(inputs)
+        inputs = self.layer3(inputs)
+        inputs = self.layer4(inputs)
+        inputs = self.layer5(inputs)
+        inputs = self.layer6(inputs)
+        inputs = self.layer7(inputs)
+        inputs = self.layer8(inputs)
+        inputs = self.layer9(inputs)
+        inputs = self.layer10(inputs)
+        inputs = self.layer11(inputs)
+        inputs = self.layer12(inputs)
+        inputs = self.layer13(inputs)
+        inputs = self.layer14(inputs)
+        return inputs
 
 
 image = Image.open("./cat.jpg")
